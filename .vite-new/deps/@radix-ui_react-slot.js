d494acccbd8ebd7fa61f2bf09b1dca9b8d6f4465
sitetamalous <sitetamalousbusnise@gmail.com>
2025-06-19 19:40:48 +0100

    Added _metadata.json

---
+++ .vite-new/deps/@radix-ui_react-slot.js
@@ -0,0 +1,14 @@
+import {
+  Root,
+  Slot,
+  Slottable
+} from "./chunk-4LMF7LFP.js";
+import "./chunk-G7Y47P27.js";
+import "./chunk-FXJVXTVJ.js";
+import "./chunk-4B2QHNJT.js";
+export {
+  Root,
+  Slot,
+  Slottable
+};
+//# sourceMappingURL=@radix-ui_react-slot.js.map
